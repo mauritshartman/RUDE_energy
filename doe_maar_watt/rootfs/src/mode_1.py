8250fe031b61d47f7befc39dd7488ced3595091f
Maurits Hartman <maurits.hartman@blocktraders.ai>
2025-11-25 12:07:55 +0000

    register reading in mode 1

--- doe_maar_watt/rootfs/src/mode_1.py
+++ doe_maar_watt/rootfs/src/mode_1.py
@@ -5,14 +5,25 @@ async def mode_1_loop():
     print(f'Mode 1 initializing')
 
     while True:
-        print(f'idle mode: connecting modbus client')
-        client = ModbusManager()
-        await client.connect()
+        try:
+            print(f'idle mode: connecting modbus client')
+            client = ModbusManager()
+            await client.connect()
 
-        print(f'idle: relinquish control and set ')
-        await client.write_registers(40149, [0, 0])  # rendement
-        await client.write_registers(40151, [0, 803])  # 803 = inactive
+            print(f'idle: relinquish control and reset power set point')
+            await client.write_registers(40149, [0, 0])  # reset rendement
+            await client.write_registers(40151, [0, 803])  # 803 = inactive
 
-        client.close()
+            print(f'idle: reading selected inverter properties:')
+            temps = await client.read_registers(30849, 'S32', device_id=3, sma_data_format='TEMP')
+            print(f'battery temperatures:')
+            for inv_name, temp in temps.items():
+                print(f'\t{inv_name}:\t{temp} {chr(176)}C')
+
+        except Exception as e:
+            print(f'idle: encountered an error: {e}')
+
+        finally:
+            client.close()
 
         await asyncio.sleep(10)
