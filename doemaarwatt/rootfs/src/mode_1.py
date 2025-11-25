f00cbff5fd142e302701804c591b9db782dba1e4
Maurits Hartman <maurits.hartman@blocktraders.ai>
2025-11-25 12:57:00 +0000

    fix

--- doemaarwatt/rootfs/src/mode_1.py
+++ doemaarwatt/rootfs/src/mode_1.py
@@ -16,9 +16,9 @@ async def mode_1_loop():
 
             print(f'idle: reading selected inverter properties:')
             temps = await client.read_registers(30849, 'S32', device_id=3, sma_data_format='TEMP')
-            print(f'battery temperatures:')
+            print(f'\tbattery temperatures:')
             for inv_name, temp in temps.items():
-                print(f'\t{inv_name}:\t{temp} {chr(176)}C')
+                print(f'\t\t{inv_name}:\t{temp} {chr(176)}C')
 
         except Exception as e:
             print(f'idle: encountered an error: {e}')
