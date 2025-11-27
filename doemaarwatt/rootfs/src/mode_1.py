7546d5915dc4feb4db05c586aa4acd753ad6a3ee
Maurits Hartman <maurits.hartman@blocktraders.ai>
2025-11-27 15:05:04 +0000

    corrected battery charge

--- doemaarwatt/rootfs/src/mode_1.py
+++ doemaarwatt/rootfs/src/mode_1.py
@@ -7,29 +7,20 @@ DM = 'Data Manager'
 async def battery_stats(inverters: ModbusManager):
     print(f'idle: reading selected inverter properties:')
 
-    print(f'\tbattery temperatures [min/average/max]:')
     temps_high = await inverters.read_registers(32221, 'S32', device_id=3, sma_data_format='TEMP')
     temps_low  = await inverters.read_registers(32227, 'S32', device_id=3, sma_data_format='TEMP')
-    for inv_name in temps_high.keys() & temps_low.keys():
+    charges = await inverters.read_registers(32233, 'U32', device_id=3, sma_data_format='FIX2')
+    voltages = await inverters.read_registers(30851, 'U32', device_id=3, sma_data_format='FIX2')
+    currents = await inverters.read_registers(30843, 'S32', device_id=3, sma_data_format='FIX3')
+
+    for inv_name in temps_high.keys() & temps_low.keys() & charges.keys() & voltages.keys() & currents.keys():
         temp_h = temps_high[inv_name]
         temp_l = temps_low[inv_name]
         temp_a = (temp_l + temp_h) / 2
-        print(f'\t\t{inv_name}:\t{temp_l} {chr(176)}C\t{temp_a} {chr(176)}C\t{temp_h} {chr(176)}C')
-
-    print(f'\tbattery charge percentage:')
-    charges = await inverters.read_registers(32233, 'U32', device_id=3, sma_data_format='FIX2')
-    for inv_name, charge in charges.items():
-        print(f'\t\t{inv_name}:\t{charge:.2f} %')
-
-    print(f'\tbattery voltage:')
-    voltages = await inverters.read_registers(30851, 'U32', device_id=3, sma_data_format='FIX2')
-    for inv_name, voltage in voltages.items():
-        print(f'\t\t{inv_name}:\t{voltage:.2f} V')
-
-    print(f'\tbattery current:')
-    currents = await inverters.read_registers(30843, 'S32', device_id=3, sma_data_format='FIX3')
-    for inv_name, current in currents.items():
-        print(f'\t\t{inv_name}:\t{current:.3f} A')
+        charge = charges[inv_name] * 10  # fix for now
+        voltage = voltages[inv_name]
+        current = currents[inv_name]
+        print(f'\t\t{inv_name}:\t{charge:.1f} %\t{voltage:.2f} V\t{current:.3f} A\t{temp_l} {chr(176)}C\t{temp_a} {chr(176)}C\t{temp_h} {chr(176)}C')
 
 
 async def data_manager_stats(dm: ModbusManager):
